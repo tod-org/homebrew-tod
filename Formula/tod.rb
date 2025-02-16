764a6405ce113902c31b30d56707f26bd37979d6
Alan Vardy <alanvardy@gmail.com>
2025-02-15 21:08:33 -0800

    try again

--- Formula/tod.rb
+++ Formula/tod.rb
@@ -4,7 +4,7 @@
 class Tod < Formula
   desc "An unofficial Todoist command line client written in Rust"
   homepage "https://github.com/alanvardy/tod"
-  url "https://github.com/alanvardy/tod/releases/latest/download/tod-mac.tar.gz"
+  url "https://github.com/alanvardy/tod/releases/download/v0.6.29/tod-mac.tar.gz"
   sha256 "437642405818cf3201c054494192e42d42d10b7c9d26c8c90a10add1d9cfdb4d"
   version "0.6.29"
 
