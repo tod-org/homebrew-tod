bbc1dcddc81c327fb34a92fac045ac278d65f02d
Jesse <stacksjb@users.noreply.github.com>
2025-06-12 22:15:39 -0600

    chore: fix homebrew Formula errors and add LInux

--- Formula/tod.rb
+++ Formula/tod.rb
@@ -1,20 +1,31 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Tod < Formula
-  desc "An unofficial Todoist command line client written in Rust"
+  desc "unofficial Todoist command-line client written in Rust"
   homepage "https://github.com/alanvardy/tod"
   version "0.8.0"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-x86_64.tar.gz"
-    sha256 "9871ac97cc14626cec3434e881638adf900e53569268c5531dd1387777e8ea78"
-  elsif Hardware::CPU.arm?
-    url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-arm64.tar.gz"
-    sha256 "7c11c25683fff0ce311486b8d434b5a98116f613628b5aeb3eaf9dfdccf508cd"
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-x86_64.tar.gz"
+      sha256 "9871ac97cc14626cec3434e881638adf900e53569268c5531dd1387777e8ea78"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-arm64.tar.gz"
+      sha256 "7c11c25683fff0ce311486b8d434b5a98116f613628b5aeb3eaf9dfdccf508cd"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-linux-x86_64.tar.gz"
+      sha256 "af4b1dee8f64dd4b1ddde3ed5a35f20e4644bdceb58e0edca673d3f1e10fbdde"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-linux-arm64.tar.gz"
+      sha256 "c57eeaee9ba9f6c6289bba07e40f7cea6b017d9f0b9c84b1ed1094e2a4efa5f4"
+    end
   end
 
   def install
     bin.install "tod"
   end
+end
+
+test do
+  assert_match "Tod #{version}", shell_output("#{bin}/tod -V")
 end
