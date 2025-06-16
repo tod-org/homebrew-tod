545c23879df0ceec202d44193f25b5200c1ea0f1
Jesse <stacksjb@users.noreply.github.com>
2025-06-16 13:30:14 -0600

    chore: manually updating to new URL format
    manually updating to new URL format

--- Formula/tod.rb
+++ Formula/tod.rb
@@ -5,18 +5,18 @@ class Tod < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-x86_64.tar.gz"
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-0.8.0-darwin-amd64.tar.gz"
       sha256 "6426123783822ea7a8a3e790e66113b98182ecc7b7a514175593b89771ee9ace"
     elsif Hardware::CPU.arm?
-      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-macOS-arm64.tar.gz"
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-0.8.0-darwin-arm64.tar.gz"
       sha256 "6426123783822ea7a8a3e790e66113b98182ecc7b7a514175593b89771ee9ace"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-v0.8.0-linux-x86_64.tar.gz"
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-0.8.0-linux-amd64.tar.gz"
       sha256 "6426123783822ea7a8a3e790e66113b98182ecc7b7a514175593b89771ee9ace"
     elsif Hardware::CPU.arm?
-      url "https://github.com/stacksjb/tod-dev/releases/download/v0.8.0/tod-0.8.0-linux-arm64.tar.gz"
+      url "https://github.com/alanvardy/tod/releases/download/v0.8.0/tod-0.8.0-linux-arm64.tar.gz"
       sha256 "6426123783822ea7a8a3e790e66113b98182ecc7b7a514175593b89771ee9ace"
     end
   end
